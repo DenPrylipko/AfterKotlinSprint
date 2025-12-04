d6582bc09b76cc7b6f6fc1e25c6d99ea8f9c41b9
DenPrylipko <denprylipko09@gmail.com>
2025-12-05 00:45:12 +0200

    AKS-17-1 User profile

---
+++ src/main/kotlin/lesson17/task1/Main.kt
@@ -0,0 +1,11 @@
+package lesson17.task1
+
+fun main() {
+    val user = UserProfile("Den", 19, "gtgt@GMAIL.COM", "2323")
+
+    user.setNewPassword("213213549")
+
+    user.email = "hello"
+
+    user.username = "a"
+}
